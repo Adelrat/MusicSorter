4cdee66c29e02d8dfc732ab10079231a0ad854ba
Адель Газизов <adellrat@mail.ru>
2020-09-09 20:42:18 +0300

    Готовая версия с установщиком

--- MusicSorter/MainWindow.xaml.cs
+++ MusicSorter/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using Microsoft.Win32;
 
 namespace MusicSorter
 {
@@ -23,6 +26,59 @@ namespace MusicSorter
         public MainWindow()
         {
             InitializeComponent();
+
+            BtnSimpleSort.Click += delegate { SimpleSorter(); };
+            BtnMediumSort.Click += delegate { MediumSort(); };
+        }
+
+        //присваивает новое название, состоящее только из цифр
+        private void SimpleSorter()
+        {
+            Sorter("Int");
+        }
+
+        //присваивает новое название, состоящее из цифр+старого названия
+        private void MediumSort()
+        {
+            Sorter("Int+old");
+        }
+
+        private void Sorter(string mode)
+        {
+            string[] paths;
+            int newNameInt;
+            string newName;
+            string[] pathMas;
+            string newPath;
+            string format;
+            string oldName;
+            //открываем проводник
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //paths = new string[openFileDialog.];
+                paths = openFileDialog.FileNames;
+                foreach (var item in paths)
+                {
+                    newNameInt = Math.Abs(Guid.NewGuid().GetHashCode() % 5000); //рандомное название от 0 до 5000
+                    newName = newNameInt.ToString();
+                    pathMas = item.Split('\\');
+                    oldName = pathMas.Last();
+                    format = item.Split('.').Last();
+                    pathMas = pathMas.Take(pathMas.Count() - 1).ToArray();
+                    newPath = String.Join("\\", pathMas);
+                    if (mode == "Int")
+                    {
+                        newPath += '\\' + newName + '.' + format;
+                    }
+                    else if (mode == "Int+old")
+                    {
+                        newPath += '\\' + newName + oldName+ '.' + format;
+                    }
+                    File.Move(item, newPath); //меняем названия 
+                }
+            }
         }
     }
 }
